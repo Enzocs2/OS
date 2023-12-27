7cca0595433a4cbf1de23e3ddaacdfcd43a9ffe6
Enzo <enzocarvalhosilva@hotmail.com>
2023-12-27 12:42:37 -0300

    aula2

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
@@ -15,14 +17,8 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
